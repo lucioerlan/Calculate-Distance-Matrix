3ed2fd968cb6b994578b4523bd32f312ba4915bb
erlanrox <luciodevinfo@gmail.com>
2021-02-01 10:42:35 -0300

    refactor separating responsibility at all

--- server/src/controllers/distance-controller.js
+++ server/src/controllers/distance-controller.js
@@ -1,70 +1,104 @@
-const db = require("../database/db-config");
-const { helper } = require("../utils/helpers");
-const _ = require("lodash");
-const path = require("path");
-const request = require("request-promise-native");
+const _ = require('lodash');
+const path = require('path');
+const request = require('request-promise-native');
+const { helper } = require('../utils/helpers');
+const Util = require('../utils/Utils');
+const { returnDocs, createDoc } = require('../models/distance-models');
 
+const util = new Util();
 
-const getFiles = async (req, res) => {
-  const result = await db("distance").orderBy("created_at", "desc").limit(4);
+/**
+ * The DistanceController.
+ *
+ * @method index get eight document
+ * @method prepare prepare for the document
+ * @method store create document as a result
+ */
 
-  try {
-    return res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ message: err });
+class DistanceController {
+  async index(req, res) {
+    try {
+      const result = await returnDocs();
+
+      if (result) {
+        util.setSuccess(200, result);
+      } else {
+        util.setError(400, 'no data!');
+      }
+      
+      return util.send(res);
+
+    } catch (err) {
+      util.setError(500, err);
+      return util.send(res);
+    }
   }
-};
 
+  
+  static async prepare(row, fullname, docFile, chooseDistances, origin, destination, rows) {
 
-const processFiles = async (req, res) => {
-  const { fullname, chooseDistances } = req.body;
-  const { filename: image } = req.file;
-
-  const raw = await helper.read(
-    path.join(`${process.env.FOLDER_PATH}/` + image)
-  );
-
-  const data = await helper.parse(raw, { relax: true });
-  const headers = data.shift();
-
-  for (let row of data) {
-    
-    const origin = headers.indexOf("Origin");
-    const destination = headers.indexOf("Destination");
-
-    const response = await request({
-      method: "GET",
+    const options = {
+      method: 'GET',
       url: `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${row[origin]}&destinations=${row[destination]}&mode=driving&key=${process.env.GOOGLE_MAPS_API_KEY}`,
       json: true,
       resolveWithFullResponse: true,
-    });
+    };
 
-    if (response.statusCode === 200) {
-      let resultado = _.get(
+    request(options, async (error, response) => {
+      if (error) throw new Error(error);
+
+      const resultado = _.get(
         response,
         `body.rows[0].elements[0].${chooseDistances}`
       );
       row.push(resultado);
+
+      const output = await helper.stringify(rows);
+      helper.write(`${process.env.FOLDER_PATH}/${docFile}`, output);
+    });
+  }
+
+
+  async store(req, res) {
+    try {
+      const { fullname, chooseDistances } = req.body;
+      const { filename: docFile } = req.file;
+
+      const raw = await helper.read(
+        path.join(`${process.env.FOLDER_PATH}/${docFile}`)
+      );
+
+      const rows = await helper.parse(raw, { relax: true });
+      const headers = rows.shift();
+
+      for (const row of rows) {
+        const origin = headers.indexOf('Origin');
+        const destination = headers.indexOf('Destination');
+
+        DistanceController.prepare(
+          row,
+          fullname,
+          docFile,
+          chooseDistances,
+          origin,
+          destination,
+          rows
+        );
+
+      }
+
+      util.setSuccess(200, 'Doc Saved!', createDoc(fullname, chooseDistances, docFile));
+
+      req.io.emit('post');
+
+      return util.send(res);
+
+    } catch (err) {
+      util.setError(500, err.message);
+      return util.send(res);
     }
-    const output = await helper.stringify(data);
-    helper.write(`${process.env.FOLDER_PATH}/` + image, output);
   }
 
-  const result = await db("distance").insert({
-    fullname,
-    chooseDistances,
-    fileName: image,
-  });
+}
 
-  try {
-    req.io.emit("post", result);
-    return res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-};
-
-module.exports = {
-  processFiles,
-  getFiles,
-};
+module.exports = DistanceController;
