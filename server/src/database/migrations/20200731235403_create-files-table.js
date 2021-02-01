73e380060132557857e404cd5080bd30e316a01b
erlanrox <luciodevinfo@gmail.com>
2021-02-01 10:36:26 -0300

    feat knex migrations

--- server/src/database/migrations/20200731235403_create-files-table.js
+++ server/src/database/migrations/20200731235403_create-files-table.js
@@ -1,13 +1,13 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('distance', function (table) {
+exports.up = (knex) => {
+  return knex.schema.createTable('distance', (table) => {
     table.increments('id').primary();
     table.string('fullname', 100);
     table.string('chooseDistances', 50);
-    table.string('fileName', 255);
+    table.string('docFile', 255);
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
 
-exports.down = function (knex) {
+exports.down = (knex) => {
   return knex.schema.dropTable('distance');
 };
