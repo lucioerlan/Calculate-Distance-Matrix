3927da7d92e446d45d9a51bcad01cac473fa8e7c
erlanrox <luciodevinfo@gmail.com>
2021-02-01 10:31:44 -0300

    style loadings

---
+++ client/src/components/Progress.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    marginTop: 20,
+  },
+  barColorPrimary: {
+    backgroundColor: '#006535',
+  },
+});
+
+export default function Progress() {
+  const classes = useStyles();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(oldProgress => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress
+        classes={{
+          barColorPrimary: classes.barColorPrimary,
+        }}
+        variant="determinate"
+        value={progress}
+      />
+    </div>
+  );
+}
