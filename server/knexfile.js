b5200ad1c433b4c856e9af1e5c8d359c1417ef73
luciorox11@outlook.com <luciodevinfo@gmail.com>
2021-03-29 06:16:08 -0300

    :whale: feat add postgres image

--- server/knexfile.js
+++ server/knexfile.js
@@ -7,7 +7,7 @@ const options = {
     connection: {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      server: process.env.DB_HOST,
+      host: process.env.DB_HOST,
       database: process.env.DB_DATABASE,
     },
     options: {
