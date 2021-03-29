39c1edf350dbc60f8b29dba5add280d45668da94
luciorox11@outlook.com <luciodevinfo@gmail.com>
2021-03-29 06:14:24 -0300

    :sparkles: feat add google url

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -9,6 +9,11 @@ const helper = {
   write: util.promisify(fs.writeFile),
 };
 
+const GoogleUrl = (row, origin, destination) => {
+  return `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${row[origin]}&destinations=${row[destination]}&mode=driving&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+};
+
 module.exports = {
-  helper
+  helper,
+  GoogleUrl,
 };
