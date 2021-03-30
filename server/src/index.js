92a35769df7a243795805d4e94fdf23881d3ad83
luciorox11@outlook.com <luciodevinfo@gmail.com>
2021-03-29 22:32:33 -0300

    :art: chore format

---
+++ server/src/index.js
@@ -0,0 +1,69 @@
+// Constants
+const PORT = process.env.PORT || 5000;
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
+const express = require('express');
+const ip = require('ip');
+const path = require('path');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+require('dotenv').config();
+require('colors');
+
+const {
+  responseMiddleware,
+  unauthorizedMiddleware,
+  securityMiddleware,
+} = require('./middlewares');
+
+const DistanceRoutes = require('./routes/distance-routes');
+const SwaggerRoutes = require('./doc/swagger-config');
+
+app.use(compression());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+app.use(responseMiddleware);
+app.use(unauthorizedMiddleware);
+securityMiddleware(app);
+
+// Routes
+app.use('/files', express.static(path.resolve(`${process.env.FOLDER_PATH}`)));
+app.use('/api', [DistanceRoutes, SwaggerRoutes]);
+
+// catch 404
+app.use((req, res) =>
+  res.parseReturn({
+    status: 404,
+    errors: [
+      {
+        message: `Invalid Route, Access http://${ip.address()}:${PORT}/api/docs`,
+      },
+    ],
+  })
+);
+
+app.use((err, req, res) => {
+  const error = app.get('env') === 'development' ? err : {};
+  const status = err.status || 500;
+  return res.status(status).json(error);
+});
+
+
+server.listen(PORT, () => {
+    console.log(
+      `Server is running at port ${PORT}, see more about the application on: http://${ip.address()}:${PORT}/api/docs`
+        .bgMagenta
+    );
+  });
+
+
+module.exports = app;
